ce6132f2635d4a8f1ec65865729680b6c91dc573
Sean Esteva <sean.esteva@kiosked.com>
2016-11-23 06:02:39 -0800

    added more partials, reformatted code a bit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express'),
     app = express();
+var config = require('./myconfig');
 
 var db,
-    url = "mongodb://localhost/test",
-    port = process.env.PORT || 3000,
-    logger = require('./logger'),
-    bodyParser = require('body-parser');
+    url = config.db.database + "://" + config.db.localhost + "/" + config.db.local_db_name,
+    mongolaburl = config.mongolab.database + "://" + config.mongolab.username + ":" + config.mongolab.password + "@" + config.mongolab.host + ":" + config.mongolab.port + "/" + config.mongolab.name;
+
+var logger = require('./logger'),
+bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger);
@@ -22,19 +24,22 @@ app.get('/route2', function (req, res) {
   res.render('view2.ejs');
 });
 app.post('/route1', function (req, res) {
+  console.log('route1 post accessed successfully');
   res.redirect('/route1');
 });
 app.post('/route2', function (req, res) {
+  console.log('route2 post accessed successfully');
   res.redirect('/route2');
 });
 
+var localurl = config.db.database + "://" + config.db.host + "/" + config.db.name;
 
 var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(url, function(err, database) {
+MongoClient.connect(localurl, function(err, database) {
   if (err)
   console.log(err)
   db = database;
-  app.listen(port, function() {
-    console.log('Magic happens on port ' + port);
+  app.listen(config.server.port, function() {
+    console.log('Magic happens on port ' + config.server.port);
   });
 });
