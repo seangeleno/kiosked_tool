2722d470796be5f50ca2418cd66d5fd8fac77b8a
Sean Esteva <sean.esteva@kiosked.com>
2016-12-04 09:19:19 -0800

    improved navbar, worked on styling, added a few more routes and resources

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(logger);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+/* GET ROUTES*/
+
 app.get('/', function (req, res) {
   res.render('index.ejs');
 });
@@ -32,6 +34,40 @@ app.get('/script_formatter', function (req, res) {
   });
 });
 
+app.get('/documentation', function (req, res) {
+  var cursor = db.collection('documentation').find().toArray(function (err, result) {
+    if (err) return console.log(err)
+    console.log(result);
+    res.render('documentation.ejs', { documentation: result });
+  });
+});
+
+app.get('/tickets', function (req, res) {
+  var cursor = db.collection('tickets').find().toArray(function (err, result) {
+    if (err) console.log(err)
+    console.log(result);
+    res.render('tickets.ejs', { tickets: result });
+  });
+});
+
+app.get('/chat', function (req, res) {
+  var cursor = db.collection('chat').find().toArray(function (err, result) {
+    if (err) console.log(err)
+    console.log(result);
+    res.render('chat.ejs', { chat: result });
+  });
+});
+
+app.get('/about', function (req, res) {
+  var cursor = db.collection('about').find().toArray(function (err, result) {
+    if (err) console.log(err)
+    console.log(result);
+    res.render('about.ejs', { about: result });
+  });
+});
+
+/*POST ROUTES*/
+
 app.post('/users', function (req, res) {
 
   console.log(req.body);
@@ -46,11 +82,52 @@ app.post('/users', function (req, res) {
 app.post('/script_formatter', function (req, res) {
 
   console.log(req.body);
-  db.collection.save(req.body, function (err, result) {
+  db.collection('script_formatter').save(req.body, function (err, result) {
     if (err) return console.log(err)
     console.log('script_formatter posted successfully!!!');
   });
   res.redirect('/script_formatter');
+});
+
+app.post('/documentation', function (req, res) {
+
+  console.log(req.body);
+
+  db.collection('documentation').save(req.body, function (err, result) {
+    if (err) return console.log(err);
+    console.log('documentation route accessed successfully!');
+  });
+  res.redirect('/documentation');
+});
+
+app.post('/tickets', function (req, res) {
+
+  console.log(req.body);
+  db.collection('tickets').save(req.body, function (err, result) {
+    if (err) return console.log(err)
+    console.log('tickets posted successfully!!!');
+  });
+  res.redirect('/tickets');
+});
+
+app.post('/chat', function (req, res) {
+
+  console.log(req.body);
+  db.collection('chat').save(req.body, function (err, result) {
+    if (err) return console.log(err)
+    console.log('chat saved successfully!!!');
+  });
+  res.redirect('/chat');
+});
+
+app.post('/about', function (req, res) {
+
+  console.log(req.body);
+  db.collection('about').save(req.body, function (err, result) {
+    if (err) return console.log(err)
+    console.log('about saved successfully!!!');
+  });
+  res.redirect('/about');
 });
 
 var MongoClient = require('mongodb').MongoClient;
